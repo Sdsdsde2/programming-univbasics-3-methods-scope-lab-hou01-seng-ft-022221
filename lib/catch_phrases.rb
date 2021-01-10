6c33141e33db66fc9badc9b09b4bd9f9bfb06c3c
Shane R <imspammails@gmail.com>
2021-01-10 04:19:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,18 +1,22 @@
+# Mario's phrase method
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
+# Toadstool's phrase method
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
+# Link's phrase method
 def link 
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
 
+# Method that calls other methods
 def all_phrases
   mario
   toadstool
